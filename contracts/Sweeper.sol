753990bbb9b70fffd0fbcef413d3e156fa21d1d6
thomashin <dn2757@gmail.com>
2018-06-19 15:14:07 +0900

    fix: latest code style

--- contracts/Sweeper.sol
+++ contracts/Sweeper.sol
@@ -1,32 +1,32 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.24;
 
 // Copyright 2017 Bittrex
 
 contract AbstractSweeper {
-    function sweep(address token, uint amount) returns (bool);
+    function sweep(address token, uint amount) public returns (bool);
 
-    function () { throw; }
+    function () public { revert(); }
 
     Controller controller;
 
-    function AbstractSweeper(address _controller) {
+    constructor(address _controller) public {
         controller = Controller(_controller);
     }
 
     modifier canSweep() {
-        if (msg.sender != controller.authorizedCaller() && msg.sender != controller.owner()) throw;
-        if (controller.halted()) throw;
+        require(msg.sender != controller.authorizedCaller() && msg.sender != controller.owner());
+        require(controller.halted());
         _;
     }
 }
 
 contract Token {
-    function balanceOf(address a) returns (uint) {
+    function balanceOf(address a) public returns (uint) {
         (a);
         return 0;
     }
 
-    function transfer(address a, uint val) returns (bool) {
+    function transfer(address a, uint val) public returns (bool) {
         (a);
         (val);
         return false;
@@ -34,11 +34,11 @@ contract Token {
 }
 
 contract DefaultSweeper is AbstractSweeper {
-    function DefaultSweeper(address controller)
-             AbstractSweeper(controller) {}
+    constructor(address controller) AbstractSweeper(controller) public {}
 
     function sweep(address _token, uint _amount)
     canSweep
+    public
     returns (bool) {
         bool success = false;
         address destination = controller.destination();
@@ -54,7 +54,7 @@ contract DefaultSweeper is AbstractSweeper {
         }
         else {
             uint amountInWei = _amount;
-            if (amountInWei > this.balance) {
+            if (amountInWei > address(this).balance) {
                 return false;
             }
 
@@ -70,19 +70,20 @@ contract DefaultSweeper is AbstractSweeper {
 
 contract UserWallet {
     AbstractSweeperList sweeperList;
-    function UserWallet(address _sweeperlist) {
+    constructor(address _sweeperlist) public {
         sweeperList = AbstractSweeperList(_sweeperlist);
     }
 
     function () public payable { }
 
-    function tokenFallback(address _from, uint _value, bytes _data) {
+    function tokenFallback(address _from, uint _value, bytes _data) public {
         (_from);
         (_value);
         (_data);
      }
 
     function sweep(address _token, uint _amount)
+    public
     returns (bool) {
         (_amount);
         return sweeperList.sweeperOf(_token).delegatecall(msg.data);
@@ -90,7 +91,7 @@ contract UserWallet {
 }
 
 contract AbstractSweeperList {
-    function sweeperOf(address _token) returns (address);
+    function sweeperOf(address _token) public returns (address);
 }
 
 contract Controller is AbstractSweeperList {
@@ -105,66 +106,66 @@ contract Controller is AbstractSweeperList {
     event LogSweep(address indexed from, address indexed to, address indexed token, uint amount);
 
     modifier onlyOwner() {
-        if (msg.sender != owner) throw;
+        require(msg.sender != owner);
         _;
     }
 
     modifier onlyAuthorizedCaller() {
-        if (msg.sender != authorizedCaller) throw;
+        require(msg.sender != authorizedCaller);
         _;
     }
 
     modifier onlyAdmins() {
-        if (msg.sender != authorizedCaller && msg.sender != owner) throw;
+        require(msg.sender != authorizedCaller && msg.sender != owner);
         _;
     }
 
-    function Controller()
+    constructor() public
     {
         owner = msg.sender;
         destination = msg.sender;
         authorizedCaller = msg.sender;
     }
 
-    function changeAuthorizedCaller(address _newCaller) onlyOwner {
+    function changeAuthorizedCaller(address _newCaller) onlyOwner public {
         authorizedCaller = _newCaller;
     }
 
-    function changeDestination(address _dest) onlyOwner {
+    function changeDestination(address _dest) onlyOwner public {
         destination = _dest;
     }
 
-    function changeOwner(address _owner) onlyOwner {
+    function changeOwner(address _owner) onlyOwner public {
         owner = _owner;
     }
 
-    function makeWallet() onlyAdmins returns (address wallet)  {
+    function makeWallet() onlyAdmins public returns (address wallet) {
         wallet = address(new UserWallet(this));
-        LogNewWallet(wallet);
+        emit LogNewWallet(wallet);
     }
 
-    function halt() onlyAdmins {
+    function halt() onlyAdmins public {
         halted = true;
     }
 
-    function start() onlyOwner {
+    function start() onlyOwner public {
         halted = false;
     }
 
     address public defaultSweeper = address(new DefaultSweeper(this));
     mapping (address => address) sweepers;
 
-    function addSweeper(address _token, address _sweeper) onlyOwner {
+    function addSweeper(address _token, address _sweeper) onlyOwner public {
         sweepers[_token] = _sweeper;
     }
 
-    function sweeperOf(address _token) returns (address) {
+    function sweeperOf(address _token) public returns (address) {
         address sweeper = sweepers[_token];
         if (sweeper == 0) sweeper = defaultSweeper;
         return sweeper;
     }
 
-    function logSweep(address from, address to, address token, uint amount) {
-        LogSweep(from, to, token, amount);
+    function logSweep(address from, address to, address token, uint amount) public {
+        emit LogSweep(from, to, token, amount);
     }
 }
